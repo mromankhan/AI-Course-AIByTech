4ea34e1646787ab1e104598f475a8a4e5a391c0f
Muhammad Roman <mromankhan005@gmail.com>
2026-08-02 00:44:06 +0500

    Variables Practice

---
+++ Python/Class-5/1-variable/variable.py
@@ -0,0 +1,52 @@
+# name = True
+
+# print(name)
+
+# name = "muzammil"
+# print("name:", name)
+# print(type(name))
+
+
+# print()
+
+
+# age = 20
+# print("age:",age)
+# print(type(age))
+
+
+# print()
+
+# city = "karachi"
+# print("city:", city)
+# print(type(city))
+
+
+# print()
+
+# height = 5.9
+# print("height:", height)
+# print(type(height))
+
+# print()
+
+# is_student = True
+# print("is_student:", is_student)
+# print(type(is_student))
+
+
+
+
+# name = "Ali"
+# print("name:", name)
+# print("name:", name)
+# print("name:", name)
+
+# print()
+
+# name = "Muzammil"
+# print("name:", name)
+# print("name:", name)
+# print("name:", name)
+# print("name:", name)
+
