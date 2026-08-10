a1aaab81de22fed94ea04ade06237b281c9c6b7b
Muhammad Roman <mromankhan005@gmail.com>
2026-08-11 00:48:49 +0500

    Input Output Practical added

---
+++ Python/Class-6/1-input-output/input-output.py
@@ -0,0 +1,34 @@
+# name = input("Enter your name:")
+# print("Welcome", name)
+
+
+
+# age = int(input("Enter your age: "))
+# print("Your age is:", age)
+# print("Your age is:", type(age))
+
+
+
+
+
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# city = input("Enter your city: ")
+
+# print(name)
+# print(age)
+# print(city)
+
+
+
+
+# number = int(input("Enter a number: "))
+
+# print(number + number)
+
+
+# print(6 + 6)
+
+# float_number = float(input("Enter a float number: "))
+# print(float_number + float_number)
